5c5a2e1243b3469fe4b7df4aed94dd5dfbba472d
laithmatalkah <laithmatalkah@yahoo.com>
2018-12-11 15:30:07 -0700

    Zuul App for Zuuldemo Service

---
+++ src/main/java/com/example/adalovelace/zuuldemo/ZuuldemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.adalovelace.zuuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuuldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuuldemoApplication.class, args);
+	}
+
+	@Bean
+	public FilterLog filterLog() {
+		return new FilterLog();
+	}
+}
